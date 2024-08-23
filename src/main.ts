be90c9b494b3b60859ffb47c05c5ed1ab23c91b9
Victor <paulo9.victor.silva@gmail.com>
2024-08-23 15:55:13 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat App API')
+    .setDescription(
+      'This API is responsible for integrate your chat application',
+    )
+    .setVersion('1.0')
+    .addServer('http://localhost:3000')
+    .addTag('User')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
